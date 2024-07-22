4c308d4d8d82fbcea2b45cf132a672fcdda752c9
Derrick wakhu shibero <shiberoderrickwakhu@gmail.com>
2024-07-22 06:08:06 +0000

    added a button for lists

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import './App.css'
 
+const letters: string = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S","T", "U", "V", "W", "X", "Y", "Z"]
+
 export default function App() {
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
+      <h1>Hangman Game</h1>
+      <div>
+        <p></p>
+        <div>
+          {letters.map((letter) => (
+            <button>{letter}</button>
+          ))}
+        </div>
+      </div>
     </main>
   )
 }
